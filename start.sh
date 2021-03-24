4ff9f3bc8259b8c51fa8a5e2a0055671faade05d
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2021-03-24 23:25:52 +0100

    update commands

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-docker service create --mode=global --name miner-local2 mining ./cpuminer -a hodl -o stratum+tcp://scrypt.eu-west.nicehash.com:3333 -u 3AnW7M1E8rLFRNrrzjYah67vFy5XdSdgxz.cloud1
+#docker service create --mode=global --name miner-local2 mining ./cpuminer -a sha256d -o stratum+tcp://sha256.eu-west.nicehash.com:3334 -u 3AnW7M1E8rLFRNrrzjYah67vFy5XdSdgxz.cloud1
+docker service create --mode=global --name miner-local mining ./cpuminer -a scrypt -o stratum+tcp://scrypt.eu-west.nicehash.com:3333 -u 3AnW7M1E8rLFRNrrzjYah67vFy5XdSdgxz.cloud1
+
+docker run -it --rm cpuminer-opt:latest -a scrypt -o stratum+tcp://scrypt.eu-west.nicehash.com:3333 -u 3AnW7M1E8rLFRNrrzjYah67vFy5XdSdgxz.worker1 -p x -t 3
+
+#https://www.nicehash.com/stratum-generator
+#https://github.com/JayDDee/cpuminer-opt
